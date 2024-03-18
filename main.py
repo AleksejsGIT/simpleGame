08a531739daecc29a5bea670494c6774906b58e9
Aleksejs Šepeļevs <118129406+AleksejsGIT@users.noreply.github.com>
2024-03-18 14:46:04 +0200

    izņemts if un izveidota replace metode

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import tkinter as tk
 import random
 
+
 class Game:
 
-    def __init__(self,length):
+    def __init__(self, length):
         self.symbols = ""
         for _ in range(length):
             if random.random() < 0.5:  # 50% chance for X, 50% chance for O
@@ -28,7 +29,7 @@ class Game:
         self.generate_button = tk.Button(self.root, text="Generate", command=self.generate_and_display)
         self.generate_button.configure(bg="#2D2327", fg="#B5C2B7")
         self.generate_button.pack()
-        self.result_label = tk.Label(self.root, text="", font=("Arial",14))
+        self.result_label = tk.Label(self.root, text="", font=("Arial", 14))
         self.result_label.configure(bg="#B5C2B7")
         self.result_label.pack()
 
@@ -40,10 +41,8 @@ class Game:
         self.computer_label.configure(bg="#B5C2B7")
         self.computer_label.pack()
 
-        #papildus ievade gājieniem tad, ja sanāk ģenerēt virkni
+        # papildus ievade gājieniem tad, ja sanāk ģenerēt virkni
         self.papildus_lauki = []
-
-
 
 
     def generate_and_display(self):
@@ -52,14 +51,14 @@ class Game:
             if 15 <= user_input <= 25:
 
                 self.result_label.config(text="Generated string: " + ''.join(self.symbols))
-                self.symbols_array = list(self.symbols) #sadala virkni masīva elementos, lai spēlētājs var veikt gājienus, izvēloties konkrētus masīva elementus
+                self.symbols_array = list(
+                    self.symbols)  # sadala virkni masīva elementos, lai spēlētājs var veikt gājienus, izvēloties konkrētus masīva elementus
                 if not self.papildus_lauki:
                     self.create_fields_for_move()
             else:
                 self.result_label.config(text="Number must be between 15 and 25.")
         except ValueError:
             self.result_label.config(text="Invalid input. Please enter a valid number.")
-
 
     def create_fields_for_move(self):
         self.input_label2 = tk.Label(self.root, text="Enter the number of the first element")
@@ -83,27 +82,23 @@ class Game:
         self.button2.pack()
         self.papildus_lauki.append(self.button2)
 
-
-
-
     def points_result(self, kartas_nr1, kartas_nr2):
         # Pārbaudīt, vai gājiens ir derīgs
-        if self.symbols_array[kartas_nr1-1] == self.symbols_array[kartas_nr2-1]:
+        if kartas_nr1 == kartas_nr2:
             return False
 
         # Rēķināt punktus
-        if self.symbols_array[kartas_nr1-1] == 'X' and self.symbols_array[kartas_nr2-1] == 'X':
+        if self.symbols_array[kartas_nr1 - 1] == 'X' and self.symbols_array[kartas_nr2 - 1] == 'X':
             self.human += 2
-        elif self.symbols_array[kartas_nr1-1] == 'X' and self.symbols_array[kartas_nr2-1] == 'O':
+        elif self.symbols_array[kartas_nr1 - 1] == 'X' and self.symbols_array[kartas_nr2 - 1] == 'O':
             self.computer -= 1
-        elif self.symbols_array[kartas_nr1-1] == 'O' and self.symbols_array[kartas_nr2-1] == 'O':
+        elif self.symbols_array[kartas_nr1 - 1] == 'O' and self.symbols_array[kartas_nr2 - 1] == 'O':
             self.computer += 2
-        elif self.symbols_array[kartas_nr1-1] == 'O' and self.symbols_array[kartas_nr2-1] == 'X':
+        elif self.symbols_array[kartas_nr1 - 1] == 'O' and self.symbols_array[kartas_nr2 - 1] == 'X':
             self.human -= 1
 
+            return True
 
-        return True
-    
     def update_points(self):
         self.human_label.configure(text="Human has " + str(self.human) + " points")
         self.computer_label.configure(text="Computer has " + str(self.computer) + " points")
@@ -122,34 +117,44 @@ class Game:
     def replace_elements(self):
         # kods, kas aizvietos divus elementus
 
-        #parveido ievadito par skaitli un -1, jo masīvā elementi sākas no 0
-        kartas_nr1 = int(self.entry2.get())-1
-        kartas_nr2 = int(self.entry3.get())-1
+        # parveido ievadito par skaitli un -1, jo masīvā elementi sākas no 0
+        kartas_nr1 = int(self.entry2.get()) - 1
+        kartas_nr2 = int(self.entry3.get()) - 1
 
-        if kartas_nr1 >= 0 and kartas_nr1 < len(self.symbols_array) and kartas_nr2 >= 0 and kartas_nr2 < len(self.symbols_array):
+        if kartas_nr1 >= 0 and kartas_nr1 < len(self.symbols_array) and kartas_nr2 >= 0 and kartas_nr2 < len(self.symbols_array) and abs(kartas_nr1 - kartas_nr2) == 1:
             if self.symbols_array[kartas_nr1] == 'X' and self.symbols_array[kartas_nr2] == 'X' or self.symbols_array[kartas_nr1] == 'X' and self.symbols_array[kartas_nr2] == 'O':
-                if abs(kartas_nr1 - kartas_nr2) == 1:
+
                     self.symbols_array[kartas_nr1] = 'O'
                     del self.symbols_array[kartas_nr2]
 
                     next_string = ''.join(self.symbols_array)
                     self.result_label.configure(text="New string: " + next_string)
 
-                    self.points_result(kartas_nr1, kartas_nr2) #izsauc metodi, kas aprēķina spēlētāju punktus
+                    self.points_result(kartas_nr1, kartas_nr2)  # izsauc metodi, kas aprēķina spēlētāju punktus
 
-                    #notīra ievades laukus
+                    # notīra ievades laukus
                     self.entry2.delete(0, tk.END)
                     self.entry3.delete(0, tk.END)
-                else:
-                    self.result_label.configure(text="error1")
             else:
-                self.result_label.configure(text="error2")
+                self.result_label.configure(text="error")
+
+            if self.symbols_array[kartas_nr1] == 'O' and self.symbols_array[kartas_nr2] == 'O' or self.symbols_array[kartas_nr1] == 'O' and self.symbols_array[kartas_nr2] == 'X':
+
+                    self.symbols_array[kartas_nr1] = 'X'
+                    del self.symbols_array[kartas_nr2]
+
+                    next_string = ''.join(self.symbols_array)
+                    self.result_label.configure(text="New string: " + next_string)
+
+                    self.points_result(kartas_nr1, kartas_nr2)  # izsauc metodi, kas aprēķina spēlētāju punktus
+
+                    # notīra ievades laukus
+                    self.entry2.delete(0, tk.END)
+                    self.entry3.delete(0, tk.END)
+            else:
+                self.result_label.configure(text="error")
         else:
-            self.result_label.configure(text="error3")
-
-
-        
-
+            self.result_label.configure(text="error")
 
     def is_over(self):
         # Pārbaude vai spēle ir beigusies
@@ -158,16 +163,11 @@ class Game:
     # def minimax(self):...
 
     def play(self):
-        #while not self.is_over(): ...
+        # while not self.is_over(): ...
         self.root.mainloop()
 
-
-
     # Run the game
+
+
 game = Game(20)
 game.play()
-
-
-
-
-
